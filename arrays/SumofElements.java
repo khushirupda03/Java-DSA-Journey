96af39c5d55b7a2b67403ae9e60bdad068047d59
khushirupda03 <khushi.rupda03@gmail.com>
2026-07-17 21:31:35 +0530

    Add files via upload

---
+++ arrays/SumofElements.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SumofElements {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of an array:");
+        int n =sc.nextInt();
+        int[]arr=new int[5];
+        System.out.println("Enter array elements: ");
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum of array elements is: " +sum);
+        sc.close();
+    }
+    
+}
